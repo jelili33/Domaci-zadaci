b9ab66b708ac66eef07e7e7e16b1b6cae2ae1136
Jelena <jilic@rocketmaiil.com>
2019-10-29 02:42:18 +0100

    Domaci Nedelja2 Ponedeljak

---
+++ Nedelja2/Domaci sreda/Prvi dan/Zadatak2js/zadatak2.js
@@ -0,0 +1,12 @@
+var name="Welcome to DOM";
+
+//id h1 je "title"
+
+document.getElementById("title").innerHTML=name;
+
+//div id=newDiv
+
+document.getElementById("newDiv").innerHTML="<p>Paragraf sa proizvoljnim tekstom</p>";
+
+
+
---
+++ Nedelja2/Domaci sreda/Prvi dan/zadatak1.js
@@ -0,0 +1,22 @@
+var a;
+a=2;
+var b;
+b=3;
+
+var zbir=a+b;
+console.log(zbir);
+
+var razlika=a-b;
+console.log(razlika);
+
+var proizvod=a*b;
+console.log(proizvod);
+
+var kolicnik=a/b;
+console.log(kolicnik);
+
+var ostatakPriDeljenju=a%b;
+console.log(ostatakPriDeljenju);
+
+var stepen=Math.pow(a,b);
+console.log(stepen);
